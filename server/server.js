d37530771aa00586114e6b5c14922da4ab5e4395
supachai.thongprasit <supachai.thongprasit@gmail.com>
2017-06-29 12:39:27 +0700

    Add DELETE /todo/:id

--- server/server.js
+++ server/server.js
@@ -64,6 +64,23 @@ app.get('/todos/:id',(req,res)=>{
 
 })
 
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findByIdAndRemove(id).then((todos)=>{
+        if(!todos){
+            return res.status(404).send()
+        }
+
+        res.send({todos})
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+})
+
 app.listen(port,()=>{
     console.log(`Started on port ${port}`)
 })
