1eee4870d258798d9c8feeb34254deeef493c85d
supachai.thongprasit <supachai.thongprasit@gmail.com>
2017-06-29 16:57:03 +0700

    Hash user password before saving

--- playground/hasing.js
+++ playground/hasing.js
@@ -1,18 +1,32 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
+var password = '123abc';
 
-var data = {
-    id:10
-}
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash)
+    })
+});
 
-var token = jwt.sign(data,'123abc')
+var hashPassword = '$2a$10$g1HH17ngC/LFyP.3FDBQcer7Xv8qH6AVGQdVJUgxbyPSAyQ508dhC';
 
-console.log(token)
+bcrypt.compare('123',hashPassword,(err,res)=>{
+    console.log(res)
+})
 
-var decoded = jwt.verify(token,'123abc')
+// var data = {
+//     id:10
+// }
 
-console.log('decoded: ',decoded)
+// var token = jwt.sign(data,'123abc')
+
+// console.log(token)
+
+// var decoded = jwt.verify(token,'123abc')
+
+// console.log('decoded: ',decoded)
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
